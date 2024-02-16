3576d6065a172d365f3cf1a380c05f74e3672536
swarna9999 <118824030+swarna9999@users.noreply.github.com>
2024-02-16 11:28:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
+    var bb = <?php echo 'kk'; ?>;
     count += 1
-    countEl.textContent = count
+    countEl.textContent = bb
 }
 
 function save() {
